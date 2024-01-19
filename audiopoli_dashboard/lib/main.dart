c2e23e4bdff7ce28bf813740e50faf2a1c5391b0
minsik <ppp37686@gmail.com>
2024-01-19 22:57:13 +0900

    add google map api, problem with key

--- audiopoli_dashboard/lib/main.dart
+++ audiopoli_dashboard/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:js' as js;
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart' as google_map_flutter;
 
-void main() {
-  runApp(const MyApp());
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +16,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String apiKey = dotenv.env['GOOGLE_MAPS_API_KEY'] ?? "API 키가 없습니다";
+    js.context.callMethod('setGoogleMapsApiKey', [apiKey]);
+
     return MaterialApp(
       home: Scaffold(
         body: Column(
@@ -31,7 +41,7 @@ class MyApp extends StatelessWidget {
                   ),
                   Expanded(
                     flex: 2,
-                    child: styledContainer(),
+                    child: mapContainer(),
                   ),
                 ],
               ),
@@ -47,7 +57,7 @@ class MyApp extends StatelessWidget {
 }
 
 class styledContainer extends StatelessWidget {
-  const styledContainer({super.key});
+  styledContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -64,6 +74,43 @@ class styledContainer extends StatelessWidget {
             offset: Offset(0, 3),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class mapContainer extends StatelessWidget {
+  mapContainer({super.key});
+  late GoogleMapController mapController;
+  GoogleMapsFlutterPlatform mapsImplementation = GoogleMapsFlutterPlatform.instance =  google_map_flutter.GoogleMapsPlugin();
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(7.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1.5,
+            blurRadius: 1.5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
       ),
     );
   }
