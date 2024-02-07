17e5af6d933482057a64eb3edb6c1370faa37df8
Minsik Kim <ppp37686@gmail.com>
2024-02-08 01:02:13 +0900

    UPDATE SoundContainer.dart : Fix toggle button

--- audiopoli_dashboard/lib/SoundContainer.dart
+++ audiopoli_dashboard/lib/SoundContainer.dart
@@ -1,10 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
-import './StyledContainer.dart';
 
 class SoundContainer extends StatefulWidget {
-  SoundContainer({super.key});
+  SoundContainer({Key? key}) : super(key: key);
 
   @override
   State<SoundContainer> createState() => _SoundContainerState();
@@ -12,9 +10,7 @@ class SoundContainer extends StatefulWidget {
 
 class _SoundContainerState extends State<SoundContainer> {
   final audioPlayer = AudioPlayer();
-  String path = './assets/audio/sample_audio.wav';
   bool isPlaying = false;
-  bool isPaused = false;
   double currentPosition = 0.0;
   double totalDuration = 0.0;
 
@@ -23,31 +19,37 @@ class _SoundContainerState extends State<SoundContainer> {
     super.initState();
     initAudioPlayer();
   }
+
   Future<void> initAudioPlayer() async {
-    await audioPlayer.setAsset(path);
-    audioPlayer.durationStream.listen((duration) {
-      setState(() {
-        totalDuration = duration?.inMilliseconds.toDouble() ?? 0.0;
+    try {
+      await audioPlayer.setAsset('assets/audio/sample_audio.wav');
+      audioPlayer.durationStream.listen((duration) {
+        setState(() {
+          totalDuration = duration?.inMilliseconds.toDouble() ?? 0.0;
+        });
       });
-    });
-    audioPlayer.positionStream.listen((position) {
-      setState(() {
-        currentPosition = position.inMilliseconds.toDouble();
+      audioPlayer.positionStream.listen((position) {
+        setState(() {
+          currentPosition = position.inMilliseconds.toDouble();
+        });
       });
-    });
+      audioPlayer.playerStateStream.listen((playerState) {
+        setState(() {
+          isPlaying = playerState.playing;
+        });
+      });
+    } catch (e) {
+      print("An error occurred: $e");
+    }
   }
 
   Future<void> togglePlayPause() async {
-    if (audioPlayer.playing) {
+    if (isPlaying) {
       await audioPlayer.pause();
     } else {
       await audioPlayer.play();
     }
-    setState(() {
-      isPlaying = audioPlayer.playing;
-    });
   }
-
 
   @override
   void dispose() {
@@ -57,31 +59,23 @@ class _SoundContainerState extends State<SoundContainer> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Music Player with Slider'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Slider(
-            value: currentPosition,
-            min: 0.0,
-            max: totalDuration,
-            onChanged: (value) async {
-              await audioPlayer.seek(Duration(milliseconds: value.toInt()));
-              setState(() {
-                currentPosition = value;
-              });
-            },
-          ),
-          IconButton(
-            icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-            iconSize: 64.0,
-            onPressed: () => togglePlayPause(),
-          ),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Slider(
+          value: currentPosition,
+          min: 0.0,
+          max: totalDuration,
+          onChanged: (value) async {
+            await audioPlayer.seek(Duration(milliseconds: value.toInt()));
+          },
+        ),
+        IconButton(
+          icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+          iconSize: 64.0,
+          onPressed: () => togglePlayPause(),
+        ),
+      ],
     );
   }
 }
